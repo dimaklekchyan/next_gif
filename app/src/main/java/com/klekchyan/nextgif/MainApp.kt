e5b435a8106f13f76868cdb2a52792c812b5a03c
Клекчян Дмитрий <dima.klekchyan@gmail.com>
2021-09-03 23:16:02 +0500

    Added required dependencies

---
+++ app/src/main/java/com/klekchyan/nextgif/MainApp.kt
@@ -0,0 +1,13 @@
+package com.klekchyan.nextgif
+
+import android.app.Application
+import timber.log.Timber
+
+class MainApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
